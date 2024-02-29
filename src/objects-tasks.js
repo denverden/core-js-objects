4cd06b9dbd73e2db2d715c5a72c274f85f7e474a
DenverDen <denverden@mail.ru>
2024-02-29 11:53:46 +0300

    feat: add result sellTickets

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -157,8 +157,25 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  const ticketPrice = 25;
+  let cash = 0;
+
+  for (let i = 0; i < queue.length; i += 1) {
+    if (queue[i] === ticketPrice) {
+      cash += ticketPrice;
+    } else {
+      const change = queue[i] - ticketPrice;
+
+      if (cash >= change) {
+        cash -= change;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return true;
 }
 
 /**
